81f068406fe529efe16498f55fbc9119c6c00ee5
Claudiu Macovei <makowey@gmail.com>
2023-09-18 13:47:19 +0300

    config pionul...

---
+++ src/component/Retailers.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let retailers = [{
+        name: "Pionul.ro",
+        logo: "https://pionul.ro/image/cache/data/Daniel/logo-final-200x56.jpg",
+        site: "https://pionul.ro"
+    }];
+</script>
+
+{#each retailers as retailer}
+    <a href="{retailer.site}" target="_blank" class="card-hover absolute">
+        <img src="{retailer.logo}" width="130" height="30" alt="{retailer.name}">
+    </a>
+{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,18 @@
     import SearchGame from "../component/SearchGame.svelte";
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
+    import Retailers from "../component/Retailers.svelte";
 
     inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-center">
     <div class="space-y-10 text-center flex flex-col items-center">
         <h2 class="h2">Boardgames Romanian Search Portal</h2>
+
+        <Retailers/>
 
         <div class="w-full">
             <SearchGame/>
