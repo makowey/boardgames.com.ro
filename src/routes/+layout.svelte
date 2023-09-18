f35623cb01a9dbb98b0cfde6ddd233a3f250b9cc
Claudiu Macovei <makowey@gmail.com>
2023-09-18 11:21:34 +0300

    update discord channel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,27 +19,11 @@
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="https://discord.gg/cybPRKu2Cb"
 					target="_blank"
 					rel="noreferrer"
 				>
 					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
 				</a>
 			</svelte:fragment>
 		</AppBar>
