fa02efd8dc98048b7ca33d20d00ea89fcf70bc2e
meo-albu <albumeo1990@gmail.com>
2023-09-19 22:10:28 +0300

    patch: create card and fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,13 @@
 <script>
-    import SearchGame from "../component/SearchGame.svelte";
-    import {dev} from '$app/environment';
-    import {inject} from '@vercel/analytics';
-    import Retailers from "../component/Retailers.svelte";
+	import SearchGame from '../component/SearchGame.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	import Retailers from '../component/Retailers.svelte';
 
-    inject({mode: dev ? 'development' : 'production'});
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center">
-    <div class="space-y-10 text-center flex flex-col items-center">
-        <div class="w-11/12 mt-3">
-            <SearchGame/>
-        </div>
-
-        <div class="w-11/12">
-            <Retailers/>
-        </div>
-    </div>
+<div class="h-full flex flex-col">
+	<SearchGame />
+	<Retailers />
 </div>
--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,46 +1,57 @@
-import {json} from '@sveltejs/kit';
-import {findRetailerByIndex} from "../../../component/retailers";
+import { json } from '@sveltejs/kit';
+import { findRetailerByIndex } from '../../../component/retailers';
+import type { Game } from '$lib/types';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export async function GET({url, fetch}) {
-    const search = url.searchParams.get('search');
-    console.log(`Search for [${search}]`)
-    if (search?.length < 3) {
-        return new Response(JSON.stringify({}), {
-            status: 403,
-            headers: {'Content-Type': 'application/json'}
-        });
-    }
+export async function GET({ url, fetch }) {
+	const search = url.searchParams.get('search');
+	console.log(`Search for [${search}]`);
+	if (search && search.length < 3) {
+		return new Response(JSON.stringify({}), {
+			status: 403,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
 
-    let data: any[] = [];
+	let data: any[] = [];
 
-    const PION: any = findRetailerByIndex('PION');
-    let response = await fetch(PION.search + search);
-    let dataPion = await response.json();
-    dataPion = [...dataPion.results.map( game => {
-        return {...game, retailer: PION}
-    })];
-    data = [...data, ...dataPion];
+	const PION: any = findRetailerByIndex('PION');
+	let response = await fetch(PION.search + search);
+	let dataPion = await response.json();
+	dataPion = [
+		...dataPion.results.map((game: Game) => {
+			return {
+				...game,
+				image: game.image.replace('50x50', '500x500'),
+				retailer: PION
+			};
+		})
+	];
+	data = [...data, ...dataPion];
 
-    const LEXSHOP: any = findRetailerByIndex('LEXSHOP');
-    response = await fetch(LEXSHOP.search + search);
-    let dataLexshop = await response.json();
-    dataLexshop = [...dataLexshop.suggestions].map(game => {
-        return {"name": game.value, "image": game.cover?.split("src=\"")[1]?.split("&crop=")[0], "url": LEXSHOP.site + game.link, "price": game.pret, retailer: LEXSHOP}
-    });
-    data = [...data, ...dataLexshop];
+	const LEXSHOP: any = findRetailerByIndex('LEXSHOP');
+	response = await fetch(LEXSHOP.search + search);
+	let dataLexshop = await response.json();
+	dataLexshop = [...dataLexshop.suggestions].map((game) => {
+		return {
+			name: game.value,
+			image: game.cover?.split('src="')[1]?.split('&crop=')[0].replace('45-45', '500-500'),
+			url: LEXSHOP.site + game.link,
+			price: game.pret,
+			retailer: LEXSHOP
+		};
+	});
+	data = [...data, ...dataLexshop];
 
-    const OZONE: any = findRetailerByIndex('OZONE');
-    response = await fetch(OZONE.search + search);
-    let dataOzone = await response.json();
-    dataOzone = [...dataOzone.items].map(game => {
-        return {"name": game.l, "image": game.t2, "url": game.u, "price": game.p, retailer: OZONE}
-    });
-    data = [...data, ...dataOzone];
+	const OZONE: any = findRetailerByIndex('OZONE');
+	response = await fetch(OZONE.search + search);
+	let dataOzone = await response.json();
+	dataOzone = [...dataOzone.items].map((game) => {
+		return { name: game.l, image: game.t2, url: game.u, price: game.p, retailer: OZONE };
+	});
+	data = [...data, ...dataOzone];
 
-    return json({
-        status: 'success',
-        games: data.sort((a, b) => parseFloat(b.price) - parseFloat(a.price))
-    });
+	return json({
+		status: 'success',
+		games: data.sort((a, b) => parseFloat(b.price) - parseFloat(a.price))
+	});
 }
