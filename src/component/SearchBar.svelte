ae099f8010fd85fee9d7b8d3e33f468ce74a9d4f
Claudiu Macovei <makowey@gmail.com>
2023-09-22 13:30:12 +0300

    setup autocomplete popup

--- src/component/SearchBar.svelte
+++ src/component/SearchBar.svelte
@@ -1,25 +1,52 @@
-<script>
-	import debounce from 'lodash.debounce';
-	import { fly } from 'svelte/transition';
+<script lang="ts">
+    import {fly} from 'svelte/transition';
+    import type {AutocompleteOption, PopupSettings} from '@skeletonlabs/skeleton';
+    import {Autocomplete, popup} from '@skeletonlabs/skeleton';
 
-	export let value = '';
+    export let value = '';
 
-	const handleInput = debounce((e) => {
-		value = e.target.value;
-	}, 500);
+    export let placeholder = 'enter your search here';
+    let popupSettings: PopupSettings = {
+        event: 'focus-click',
+        target: 'popupAutocomplete',
+        placement: 'bottom',
+    };
 
-	export let placeholder = 'enter your search here';
+    const suggestions: AutocompleteOption[] = [
+        {
+            label: 'Azul',
+            value: 'Azul',
+            keywords: 'boardgame'
+        },
+        {
+            label: 'Brass',
+            value: 'Brass Birmingham',
+            keywords: 'boardgame'
+        }];
+
+    function onSuggestionSelection(event: CustomEvent<AutocompleteOption>): void {
+        value = event.detail.label;
+    }
 </script>
 
 <div class="flex justify-center py-6" in:fly={{ y: -100, duration: 500, delay: 1000 }}>
-	<form name="search">
-		<input
-			class="p-4 border-2 border-solid border-yellow-400 bg-transparent rounded-[30px] min-w-[300px] hover:rounded-lg transition-all duration-200 focus:outline-none"
-			name="q"
-			{placeholder}
-			onmouseout="this.value = ''; this.blur();"
-			on:input={handleInput}
-			autocomplete="off"
-		/>
-	</form>
+    <form name="search">
+        <input
+                type="search"
+                class="p-4 border-2 border-solid border-yellow-400 bg-transparent rounded-[30px] min-w-[300px] hover:rounded-lg transition-all duration-200 focus:outline-none input autocomplete"
+                name="q"
+                {placeholder}
+                autocomplete="off"
+                bind:value
+                use:popup={popupSettings}
+        />
+
+        <div data-popup="popupAutocomplete" class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto z-10" tabindex="-1">
+            <Autocomplete
+                    bind:input={value}
+                    options={suggestions}
+                    on:selection={onSuggestionSelection}
+            />
+        </div>
+    </form>
 </div>
