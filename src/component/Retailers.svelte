d925d257f568ee5ec890886bef379b933a8eb925
Claudiu Macovei <makowey@gmail.com>
2023-09-25 12:26:09 +0300

    flex-wrap logos

--- src/component/Retailers.svelte
+++ src/component/Retailers.svelte
@@ -2,7 +2,7 @@
 	import { retailers } from '$lib/retailers';
 </script>
 
-<footer class="mt-auto p-6 flex justify-center items-center gap-10">
+<footer class="mt-auto p-6 flex justify-center items-center gap-10 flex-wrap">
 	{#each retailers as retailer}
 		<a href={retailer.site} target="_blank">
 			<img src={retailer.logo} alt={retailer.name} class="w-32" />
