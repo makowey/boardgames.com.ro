962861a5332e4c166a040406b744cef8c4679829
Claudiu Macovei <makowey@gmail.com>
2023-09-27 15:57:43 +0300

    loading animations...

--- src/component/Games.svelte
+++ src/component/Games.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
-	import LottieAnimation from './player/LottieAnimation.svelte';
-	import GameCard from './GameCard.svelte';
-	import type { Game } from '$lib/types';
+    import LottieAnimation from './player/LottieAnimation.svelte';
+    import GameCard from './GameCard.svelte';
+    import type {Game} from '$lib/types';
+    import {loadingAnimations} from "$lib/stores.js";
 
-	export let games: Game[];
-	export let searchText = 'boardgame';
-	let isNavigating = false;
-	let executionTime = 0;
-	export let numberOfMinimCharsForSearch = 3;
+    export let games: Game[];
+    export let searchText = 'boardgame';
+    let isNavigating = false;
+    let executionTime = 0;
+    export let numberOfMinimCharsForSearch = 3;
 
-	async function searchForGame() {
-		isNavigating = true;
-		const response = await fetch('/api/search?search=' + searchText);
-		const data = await response.json();
-		games = [...data.games];
-		executionTime = data.executionTime;
-		isNavigating = false;
-		// console.log(`Found ${games.length} games...`, games);
-	}
+    async function searchForGame() {
+        isNavigating = true;
+        const response = await fetch('/api/search?search=' + searchText);
+        const data = await response.json();
+        games = [...data.games];
+        executionTime = data.executionTime;
+        isNavigating = false;
+        // console.log(`Found ${games.length} games...`, games);
+    }
 
-	$: if (searchText?.length >= numberOfMinimCharsForSearch) {
-		searchForGame();
-	}
+    $: if (searchText?.length >= numberOfMinimCharsForSearch) {
+        searchForGame();
+    }
 </script>
 
 {#if isNavigating}
-	<LottieAnimation path="loading" />
+    <LottieAnimation path={$loadingAnimations[Math.floor(Math.random() * $loadingAnimations.length)]}/>
 {:else if games?.length > 0}
-	<ul class="grid lg:grid-cols-2 gap-8">
-		{#each games as game}
-			<GameCard {game} />
-		{/each}
-	</ul>
-	<p class="text-xs text-right text-cyan-800">{executionTime}ms</p>
+    <ul class="grid lg:grid-cols-2 gap-8">
+        {#each games as game}
+            <GameCard {game}/>
+        {/each}
+    </ul>
+    <p class="text-xs text-right text-cyan-800">{executionTime}ms</p>
 {:else if searchText}
-	<LottieAnimation />
+    <LottieAnimation/>
 {/if}
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,5 @@
-import {writable} from 'svelte/store';
+import {readable, writable} from 'svelte/store';
 
 export const isMobile = writable(false);
+
+export const loadingAnimations = readable(["loading", "circleLoading", "handLoading", "triangleLoading", "search"]);
