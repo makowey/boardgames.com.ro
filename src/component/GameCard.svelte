a215b0488cb5e0955091349016ad8b01f6642d5f
Claudiu Macovei <makowey@gmail.com>
2023-09-28 09:44:47 +0300

    another ribbon...

--- src/component/GameCard.svelte
+++ src/component/GameCard.svelte
@@ -4,7 +4,7 @@
     export let game: Game;
 </script>
 
-<li class="card card-hover flex flex-col sm:flex-row shadow-2xl overflow-hidden rounded-lg">
+<li class="card card-hover flex flex-col sm:flex-row shadow-2xl overflow-hidden rounded-lg box">
     <div class="relative aspect-video overflow-hidden sm:aspect-square sm:max-w-[270px] bg-white">
         <img
                 class="object-contain sm:object-cover w-full h-full"
@@ -12,9 +12,7 @@
                 alt="Game thumbnail"
         />
         {#if game?.promotion && game.promotion > 0}
-            <div class="ribbon">
-                <span class="ribbon__content font-bold">{game.promotion}%</span>
-            </div>
+            <div class="ribbon ribbon-top-left animate-pulse"><span>{game.promotion}%</span></div>
         {/if}
     </div>
     <div class="card-body p-6 flex flex-col gap-3 flex-1">
@@ -33,54 +31,67 @@
 </li>
 
 <style>
+    .box {
+        position: relative;
+        box-shadow: 0 0 15px rgba(0, 0, 0, .1);
+    }
+
+    /* common */
     .ribbon {
         width: 150px;
         height: 150px;
         overflow: hidden;
         position: absolute;
-        top: -10px;
-        right: -10px;
-
     }
 
-    .ribbon__content {
-        left: -25px;
-        top: 30px;
-        -webkit-transform: rotate(45deg);
-        -ms-transform: rotate(45deg);
-        transform: rotate(45deg);
+    .ribbon::before,
+    .ribbon::after {
+        position: absolute;
+        z-index: -1;
+        content: '';
+        display: block;
+        border: 5px solid #2980b9;
+    }
+
+    .ribbon span {
         position: absolute;
         display: block;
         width: 225px;
-        padding: 10px 0;
-        background-color: #00aaa0;
-        box-shadow: 0 0px 10px;
+        padding: 15px 0;
+        background-color: #3498db;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, .1);
         color: #fff;
+        font: 700 18px/1 'Lato', sans-serif;
         text-shadow: 0 1px 1px rgba(0, 0, 0, .2);
         text-transform: uppercase;
         text-align: center;
-        border: 2px dotted #fff;
-        outline: 5px solid #f5cd79;
     }
 
-    .ribbon::before {
+    /* top left*/
+    .ribbon-top-left {
+        top: -10px;
+        left: -10px;
+    }
+
+    .ribbon-top-left::before,
+    .ribbon-top-left::after {
+        border-top-color: transparent;
+        border-left-color: transparent;
+    }
+
+    .ribbon-top-left::before {
         top: 0;
-        left: 0;
-        position: absolute;
-        z-index: -1;
-        content: '';
-        display: block;
-        border: 5px solid #f19066;
-        box-sizing: content-box;
+        right: 0;
     }
 
-    .ribbon::after {
+    .ribbon-top-left::after {
         bottom: 0;
-        right: 0;
-        position: absolute;
-        z-index: -1;
-        content: '';
-        display: block;
-        border: 5px solid #f19066;
+        left: 0;
+    }
+
+    .ribbon-top-left span {
+        right: -25px;
+        top: 30px;
+        transform: rotate(-45deg);
     }
 </style>
