da4ad00413dbc38e58d533fc98e920fc9cfc46a1
Claudiu Macovei <makowey@gmail.com>
2023-10-28 10:47:17 +0300

    add new fonts

--- src/component/GameCard.svelte
+++ src/component/GameCard.svelte
@@ -17,7 +17,7 @@
         <img src={game.retailer.logo} alt={game.retailer.name}
              class="w-24 px-1.5 py-0.5 bg-gray-500 bg-opacity-66 rounded-3xl"/>
         <h2 class="card-title text-left font-bold">{@html game.name}</h2>
-        <div class="badge bg-violet-400 rounded-full max-w-max text-black">{@html game.price}</div>
+        <div class="badge bg-violet-400 rounded-full max-w-max text-black text-xl">{@html game.price}</div>
         <div class="flex justify-end mt-auto w-full">
             <a
                     href={game.url}
