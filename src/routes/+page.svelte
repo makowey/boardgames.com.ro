244f7dcfc888391e39eaa1d0139063cf37b2bb17
Claudiu Macovei <makowey@gmail.com>
2023-12-14 10:26:35 +0200

    mobile galery...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,30 @@
 <script>
 	import SearchGame from '../component/SearchGame.svelte';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
+	import {dev} from '$app/environment';
+	import {inject} from '@vercel/analytics';
 	import Retailers from '../component/Retailers.svelte';
 	import Gallery from "../component/Gallery.svelte";
 	import {retailers} from "$lib/retailers";
+	import {isMobile} from "$lib/stores";
 
-	inject({ mode: dev ? 'development' : 'production' });
+	inject({mode: dev ? 'development' : 'production'});
 </script>
 
 <div class="h-full flex flex-col">
-	<SearchGame />
-	<Gallery anchor="retailers" title="Retailers" games={retailers.map(r => {
+    <SearchGame/>
+
+    {#if isMobile}
+        <Gallery anchor="retailers" title="APIs" games={retailers.map(r => {
 		return {
 			name: ' ',
 			url: r.site,
 			image: r.logo,
 			thumbnail: r.logo,
 			rank: ' ',
-
 		}
 	})}/>
-<!--	<Retailers />-->
+    {:else }
+        <Retailers/>
+    {/if}
+
 </div>
