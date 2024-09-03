a843485820c5a0d8dd034fdef844fc60bd201407
Claudiu Macovei <makowey@gmail.com>
2024-09-03 09:08:53 +0300

    update most played

--- src/routes/api/bgg/mostplayed/+server.ts
+++ src/routes/api/bgg/mostplayed/+server.ts
@@ -7,8 +7,8 @@ import {json} from "@sveltejs/kit";
 
 export async function GET({fetch}) {
 
-    const currentMonthID: number = 335065;
-    const currentMonthName: string = 'FEB';
+    const currentMonthID: number = 337879;
+    const currentMonthName: string = 'MAY';
 
     console.log(`Search for [mostplayed list for ${currentMonthName}] on BGG`);
     const response1 = await fetch(`https://api.geekdo.com/api/listitems?page=1&listid=${currentMonthID}`);
