434533caa73100c1fdee514a036d429d2723aa53
Claudiu Macovei <makowey@gmail.com>
2025-01-09 10:18:00 +0200

    play takeiteasy...

---
+++ src/routes/play/takeiteasy/+page.svelte
@@ -0,0 +1,9 @@
+<div class="row-container">
+	<div class="first-row"></div>
+	<iframe src="https://takeiteasy-game.vercel.app" title="Take it Easy - Boardgame" class="second-row"/>
+</div>
+
+<style>
+    .row-container {display: flex; width: 100%; height: 100%; flex-direction: column; background-color: blue; overflow: hidden;}
+    .second-row { flex-grow: 1; border: none; margin: 0; padding: 0; }
+</style>
